18f537341311da9e2bbcaf8d6f2bcc05a8fdf91e
MonkeyGod6135 <dwan.conyers@gmail.com>
2022-03-29 10:34:02 -0400

    Working on Viewing and creating Shopping List

--- app/controllers/shoppers_controller.rb
+++ app/controllers/shoppers_controller.rb
@@ -1,4 +1,24 @@
 class ShoppersController < ApplicationController
 	def index
+		@shoppers = Shoppinglist.all
 	end 
+
+	def new
+	end
+
+	def create
+		shopper = Shoppinglist.new(shopper_params)
+		if shopper.save
+			redirect_to "/shoppers"
+		else
+			redirect_to "/shoppers/new"
+		end
+	end
+
+	private
+	def shopper_params
+		# params is a Rails object that gets the specified request
+		# parameters
+		params.require(:shopper).permit(:shopping_list_name, :store, :date)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "shoppers" => "shoppers#index"
+  get "shoppers/new" => "shoppers#new"
+  post "shoppers" => "shoppers#create"
 end
